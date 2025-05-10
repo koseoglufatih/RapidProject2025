bfeaca0b7c66d8aef22da59b85c04ea6c9495018
FatihKoseoglu <koseoglufatih@gmail.com>
2025-05-10 22:18:37 +0300

    class tanıtım

--- ApiConsume/HotelProject.EntityLayer/Concrete/Room.cs
+++ ApiConsume/HotelProject.EntityLayer/Concrete/Room.cs
@@ -8,5 +8,14 @@ namespace HotelProject.EntityLayer.Concrete
 {
     public class Room
     {
+        public int RoomId { get; set; }
+        public string RoomNumber { get; set; }
+        public string RoomCoverImage { get; set; }
+        public int Price { get; set; }
+        public string Title { get; set; }
+        public string BadCount { get; set; }
+        public string BathCount { get; set; }
+        public string Wifi { get; set; }
+        public string Description { get; set; }
     }
 }
