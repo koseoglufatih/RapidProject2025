4800939945e1b92e68ea9025e94ef45ee7a80a6e
FatihKoseoglu <koseoglufatih@gmail.com>
2025-05-13 15:02:32 +0300

    registerdto

--- Frontend/HotelProject.WebUI/Controllers/RegisterController.cs
+++ Frontend/HotelProject.WebUI/Controllers/RegisterController.cs
@@ -1,9 +1,24 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using HotelProject.EntityLayer.Concrete;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 
 namespace HotelProject.WebUI.Controllers
 {
     public class RegisterController : Controller
     {
+        private readonly UserManager<AppUser> _userManager;
+
+        public RegisterController(UserManager<AppUser> userManager)
+        {
+            _userManager = userManager;
+        }
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
         public IActionResult Index()
         {
             return View();
---
+++ Frontend/HotelProject.WebUI/Dtos/RegisterDto/CreateNewUserDto.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HotelProject.WebUI.Dtos.RegisterDto
+{
+    public class CreateNewUserDto
+    {
+        [Required(ErrorMessage ="Ad Alanı Gereklidir")]
+        public string Name { get; set; }
+        
+        [Required(ErrorMessage = "Soyad Alanı Gereklidir")]
+        public string Surname { get; set; }
+
+        [Required(ErrorMessage = "Kullanıcı Alanı Gereklidir")]
+        public string Username { get; set; }
+
+        [Required(ErrorMessage = "Mail Alanı Gereklidir")]
+        public string Mail { get; set; }
+
+        [Required(ErrorMessage = "Şifre Alanı Gereklidir")]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "Şifre Tekrar Alanı Gereklidir")]
+        [Compare("Password",ErrorMessage ="Şifreler aynı olmalıdır")]
+        public string ConfirmPassword { get; set; }
+    }
+}
