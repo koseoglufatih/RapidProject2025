94b7381021e55d578517eba3c65de1228c5f5b5d
FatihKoseoglu <koseoglufatih@gmail.com>
2025-05-13 14:13:17 +0300

    serviceconroller

--- Frontend/HotelProject.WebUI/Startup.cs
+++ Frontend/HotelProject.WebUI/Startup.cs
@@ -20,6 +20,7 @@ namespace HotelProject.WebUI
         {
             services.AddHttpClient();
             services.AddControllersWithViews();
+            services.AddAutoMapper(typeof(Startup));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
