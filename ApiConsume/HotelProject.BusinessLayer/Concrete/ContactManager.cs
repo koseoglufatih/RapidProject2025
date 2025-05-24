0a153d201a9e1e2e6c987d43916d4de83e3d70e5
FatihKoseoglu <koseoglufatih@gmail.com>
2025-05-24 16:52:27 +0300

    contact

---
+++ ApiConsume/HotelProject.BusinessLayer/Concrete/ContactManager.cs
@@ -0,0 +1,46 @@
+﻿using HotelProject.BusinessLayer.Abstract;
+using HotelProject.DataAccessLayer.Abstract;
+using HotelProject.EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelProject.BusinessLayer.Concrete
+{
+    public class ContactManager : IContactService
+    {
+        private readonly IContactDal _contactDal;
+
+        public ContactManager(IContactDal contactDal)
+        {
+            _contactDal = contactDal;
+        }
+
+        public Contact GetById(int id)
+        {
+            return _contactDal.GetById(id);
+        }
+
+        public void TDelete(Contact t)
+        {
+           _contactDal.Delete(t);
+        }
+
+        public List<Contact> TGetList()
+        {
+           return _contactDal.GetList();
+        }
+
+        public void TInsert(Contact t)
+        {
+            _contactDal.Insert(t);
+        }
+
+        public void Update(Contact t)
+        {
+            _contactDal.Update(t);
+        }
+    }
+}
