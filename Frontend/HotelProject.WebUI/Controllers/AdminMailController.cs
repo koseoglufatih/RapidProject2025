e7358e8d30eca62782e58aa18f20812d09c76dff
FatihKoseoglu <koseoglufatih@gmail.com>
2025-05-28 12:23:07 +0300

    sendmail-2

--- Frontend/HotelProject.WebUI/Controllers/AdminMailController.cs
+++ Frontend/HotelProject.WebUI/Controllers/AdminMailController.cs
@@ -1,6 +1,8 @@
 ﻿using HotelProject.WebUI.Models.Mail;
+using MailKit.Net.Smtp;
 using Microsoft.AspNetCore.Mvc;
 using MimeKit;
+
 
 namespace HotelProject.WebUI.Controllers
 {
@@ -16,6 +18,7 @@ namespace HotelProject.WebUI.Controllers
         public IActionResult Index(AdminMailViewModel model)
         {
             MimeMessage mimeMessage = new MimeMessage();
+
             MailboxAddress mailboxAddressFrom = new MailboxAddress("HotelierAdmin", "burası bizim mailimiz");
             mimeMessage.From.Add(mailboxAddressFrom);
 
@@ -23,7 +26,18 @@ namespace HotelProject.WebUI.Controllers
             mimeMessage.To.Add(mailboxAddressTo);
 
             var bodyBuilder= new BodyBuilder();
-            
+            bodyBuilder.TextBody = model.Body;
+            mimeMessage.Body = bodyBuilder.ToMessageBody();
+
+            mimeMessage.Subject = model.Subject;
+
+            SmtpClient smtpClient = new SmtpClient();
+            smtpClient.Connect("smtp.gmail.com",587,false);
+            smtpClient.Authenticate("mail", "password key");
+            smtpClient.Send(mimeMessage);
+            smtpClient.Disconnect(true);
+            return View();
+
 
             return View();
         }
