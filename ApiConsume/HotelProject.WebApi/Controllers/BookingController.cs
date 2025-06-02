3516c6b291e233ebd36528127fed1c8c76dc9118
FatihKoseoglu <koseoglufatih@gmail.com>
2025-06-02 14:57:36 +0300

    last6bookings-2

--- ApiConsume/HotelProject.WebApi/Controllers/BookingController.cs
+++ ApiConsume/HotelProject.WebApi/Controllers/BookingController.cs
@@ -64,6 +64,12 @@ namespace HotelProject.WebApi.Controllers
             _bookingService.TBookingStatusChangeApproved2(id);
             return Ok();
         }
+        [HttpGet("Last6Booking")]
+        public IActionResult Last6Booking()
+        {
+            var values = _bookingService.TLast6Bookings();
+            return Ok(values);
+        }
     }
 }
 
