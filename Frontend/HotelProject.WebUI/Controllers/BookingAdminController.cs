bfa42b6206b72c6318361f344362ffc5b23bbbcb
FatihKoseoglu <koseoglufatih@gmail.com>
2025-06-03 16:54:48 +0300

    updatebooking

--- Frontend/HotelProject.WebUI/Controllers/BookingAdminController.cs
+++ Frontend/HotelProject.WebUI/Controllers/BookingAdminController.cs
@@ -81,6 +81,20 @@ namespace HotelProject.WebUI.Controllers
         }
 
 
+        [HttpPost]
+        public async Task<IActionResult> UpdateBooking(UpdateBookingDto updateBookingDto)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var jsonData = JsonConvert.SerializeObject(updateBookingDto);
+            StringContent stringContent = new StringContent(jsonData, Encoding.UTF8, "application/json");
+            var responseMessage = await client.PutAsync("http://localhost:56810/api/Booking/UpdateBooking", stringContent);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
         public async Task<IActionResult> ApprovedReservation(ApprovedReservationDto dto)
         {
             var client = _httpClientFactory.CreateClient();
